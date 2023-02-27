ea178971dfbdaee238ab2205111cba377653a6f8
JonasAmiri <jamiri17@student.aau.dk>
2023-02-27 12:55:38 +0100

    MovieClassWithManyToMany

---
+++ src/main/java/com/example/hibernatedb/model/Movie.java
@@ -0,0 +1,139 @@
+package com.example.hibernatedb.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Movie")
+@Table(name = "movie")
+public class Movie {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+    @Column(name = "title",
+    nullable = false,
+    columnDefinition = "TEXT")
+
+    private String title;
+
+    @Column(name = "genre",
+            nullable = false,
+            columnDefinition = "TEXT")
+    private String genre;
+
+    @Column(name = "realise",
+    nullable = false)
+    private Integer realise;
+
+    @Column(name = "director" ,
+            nullable = false,
+            columnDefinition = "TEXT")
+
+    private String Director;
+
+    @Column(name = "picture",
+            nullable = false,
+            columnDefinition = "TEXT")
+
+    private String picture;
+
+    @Column(name = "trailer",
+            nullable = false,
+            columnDefinition = "TEXT")
+    private String trailer;
+
+    @ManyToMany
+
+    private Character character;
+
+    public Movie(Long id, String title, String genre, Integer realise, String director, String picture, String trailer) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.realise = realise;
+        Director = director;
+        this.picture = picture;
+        this.trailer = trailer;
+    }
+
+    public Movie() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getRealise() {
+        return realise;
+    }
+
+    public void setRealise(Integer realise) {
+        this.realise = realise;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", realise=" + realise +
+                ", Director='" + Director + '\'' +
+                ", picture='" + picture + '\'' +
+                ", trailer='" + trailer + '\'' +
+                '}';
+    }
+}
+
+
