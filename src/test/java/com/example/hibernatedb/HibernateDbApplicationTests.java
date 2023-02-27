80ceb5cc5a1b4c1c8af1432720ecb5c40d384521
Xarunah <nadja.c.olsen@gmail.com>
2023-02-27 09:31:33 +0100

    Initial commit

---
+++ src/test/java/com/example/hibernatedb/HibernateDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernatedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
