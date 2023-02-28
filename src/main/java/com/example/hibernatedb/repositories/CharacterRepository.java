e940f60af7aedbf283ae814c72540356f252ebd5
Xarunah <nadja.c.olsen@gmail.com>
2023-02-28 10:13:05 +0100

    fixed characterrepository

--- src/main/java/com/example/hibernatedb/repositories/CharacterRepository.java
+++ src/main/java/com/example/hibernatedb/repositories/CharacterRepository.java
@@ -1,5 +1,6 @@
 package com.example.hibernatedb.repositories;
 
+import com.example.hibernatedb.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
