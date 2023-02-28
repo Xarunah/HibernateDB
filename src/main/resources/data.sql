42338974d3b4a512540e4de340241d05016af325
JonasAmiri <jamiri17@student.aau.dk>
2023-02-28 11:36:02 +0100

    data.sql with 3franchise and 3 movies is insertet

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO franchise (franchise_id, description, name)
+values (1,'dummydescription', 'Marvel');
+INSERT INTO franchise (franchise_id, description, name)
+values (2,'dummy2description', 'Disney');
+INSERT INTO franchise (franchise_id, description, name)
+values (3,'dummy3description', 'Avengers');
+
+INSERT INTO movie(movie_id, director, genre, movie_picture, realise, title, trailer, franchise_id)
+VALUES (1 , 'JohnWho', 'Fantasy', 'www.picture.com', 1999, 'Predator','www.trailer.com', 1 );
+
+INSERT INTO movie(movie_id, director, genre, movie_picture, realise, title, trailer, franchise_id)
+VALUES (2 , 'JamesCameron', 'Action', 'www.picture2.com', 2022, 'Avatar','www.trailer2.com', 2 );
+
+INSERT INTO movie(movie_id, director, genre, movie_picture, realise, title, trailer, franchise_id)
+VALUES (3 , 'RyanCoogler', 'Action', 'www.picture3.com', 2022, 'BlackPanther2','www.trailer3.com', 3 );
+
